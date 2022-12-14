400b4ddc1b5fe0dc5f2f8c0c58c59842a065d3e8
luvoid <72511739+luvoid@users.noreply.github.com>
2022-12-14 16:49:29 -0500

    Update docs generation

--- docs/bpydoc.py
+++ docs/bpydoc.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+from ctypes import pointer
+from enum import Enum
 
 import inspect
 from inspect import Parameter
@@ -7,8 +9,9 @@ import traceback
 import warnings
 from collections.abc import Callable
 import typing
-from typing import Any, Iterable, Set, Tuple, TypeVar, List, TypeAlias
+from typing import Any, Iterable, Tuple, Type, TypeVar, TypeAlias
 from types import ModuleType
+from weakref import ReferenceType
 
 from pdoc._compat import cache, cached_property
 from pdoc import doc_ast, extract
@@ -24,13 +27,13 @@ InfoNamespaceRNA: TypeAlias = InfoStructRNA | InfoOperatorRNA
 InfoCallableRNA = (InfoFunctionRNA, InfoOperatorRNA)
 
 
-class RnaNamespace():
+class RnaNamespace(Namespace[T], metaclass=ABCMeta):
     def __init__(self, modulename, qualname):
         self.modulename = modulename
         self.qualname = qualname
 
     @staticmethod
-    def object_from_rna_info(rna_member: InfoMemberRNA) -> Tuple[str, object]:
+    def _object_from_rna_info(rna_member: InfoMemberRNA) -> Tuple[str, object]:
         name = rna_member.identifier
         obj = rna_member
         if isinstance(rna_member, InfoStructRNA):
@@ -54,14 +57,15 @@ class RnaNamespace():
     
     @cached_property
     def bpy_members(self) -> dict[str, Doc]:
-        members: dict[str, Doc] = {}
+        doc_members: dict[str, Doc] = {}
+        bpy_members: dict[str, Doc] = {}
         for name, obj in self._rna_member_objects.items():
             qualname = f"{self.qualname}.{name}".lstrip(".")
             taken_from = self._taken_from(name, obj)
             doc: Doc[Any] = None
             if isinstance(obj, Doc):
                 # It's already a doc object so just forward it
-                members[obj.name] = obj
+                doc_members[obj.name] = obj
             elif isinstance(obj, bpy.ops._BPyOpsSubModOp):
                 doc = BpyFunction(self.modulename, qualname, obj, taken_from)
             elif isinstance(obj, (bpy.types.Property)):
@@ -75,12 +79,32 @@ class RnaNamespace():
                 doc = BpyClass(self.modulename, qualname, obj, taken_from)
                 
             if not doc is None:
-                members[doc.name] = doc
+                bpy_members[doc.name] = doc
 
-        return members
+        sorted_doc_members = dict(sorted(doc_members.items()))
+        sorted_bpy_members = dict(sorted(bpy_members.items()))
+        return sorted_doc_members | sorted_bpy_members
 
     def _taken_from(self, member_name: str, obj: Any) -> tuple[str, str]:
-        return (self.modulename, self.qualname)
+        modulename = self.modulename
+        qualname = self.qualname
+        if isinstance(obj, (bpy.types.Property)):
+            if hasattr(self, 'obj'):
+                modulename = self.obj.__module__
+                if hasattr(self.obj, '__qualname__'):
+                    qualname = self.obj.__qualname__
+            if hasattr(self, 'rna_struct'):
+                struct_info = InfoStructRNA.global_lookup.get(('', self.rna_struct.identifier), None)
+                if struct_info is not None:
+                    modulename = struct_info.module_name
+                    qualname = struct_info.identifier
+        elif not obj is None:
+            modulename = obj.__module__
+            if hasattr(obj, '__qualname__'):
+                qualname = obj.__qualname__
+            elif hasattr(obj, '__name__'):
+                qualname = obj.__name__
+        return (modulename, qualname)
     
     @abstractmethod
     def members(self) -> dict[str, Doc]:
@@ -99,25 +123,26 @@ class RnaInfo(RnaNamespace):
         rna_member_info: Iterable[InfoMemberRNA] = []
     ):
         RnaNamespace.__init__(self, modulename, qualname)
-        self.rna_member_info: Set[InfoMemberRNA] = set(rna_member_info)
+        self.rna_member_info: set[InfoMemberRNA] = set(rna_member_info)
 
     @cached_property
-    def _rna_member_objects(self) -> dict[str, Any]:
+    def _rna_member_objects(self, debugprint=False) -> dict[str, Any]:
         members = {}
         for rna_member in self.rna_member_info:
             if isinstance(rna_member, Doc):
+                if debugprint:
+                    print(f"Got doc {rna_member}")
                 name = rna_member.name
                 obj = rna_member
             else:
-                name, obj = self.object_from_rna_info(rna_member)
+                name, obj = self._object_from_rna_info(rna_member)
             members[name] = obj
         return members
 
 
-class RnaStruct(RnaNamespace, Class):
+class RnaStruct(RnaNamespace):
     def __init__(self, modulename, qualname, rna_struct, taken_from):
         RnaNamespace.__init__(self, modulename, qualname)
-        Class.__init__(self, modulename, qualname, rna_struct, taken_from)
         self.rna_struct = rna_struct
     
     @cached_property
@@ -144,43 +169,86 @@ class RnaStruct(RnaNamespace, Class):
 
     @cached_property
     def bases(self) -> list[tuple[str, str, str]]:
-        return []
-        bases = [self.rna_struct.base]
-        return bases
+        base_rna_struct = self.rna_struct.base
+        base_rna_info = InfoStructRNA.global_lookup.get(('', base_rna_struct.identifier), None)
+        if not base_rna_info is None:
+            return [(base_rna_info.module_name, base_rna_info.identifier, f'{base_rna_info.module_name}.{base_rna_info.identifier}')]
+        else:
+            return []
 
 
-class BpyModule(RnaInfo, Module):
+
+class BpyNamespace(RnaInfo, Namespace[None]):
+    kind = "module"
+
     def __init__(
         self,
-        module: types.ModuleType,
+        namespace_name: str,
         rna_member_info: Iterable[(InfoMemberRNA, Doc)]
     ):
-        Module.__init__(self, module)
-        RnaInfo.__init__(self, module.__name__, '', rna_member_info)
-        self.bpy_module = module
+        RnaInfo.__init__(self, namespace_name, '', rna_member_info)
+        Doc.__init__(self, namespace_name, '', None, (namespace_name, ''))
+
+    @cache
+    @_include_fullname_in_traceback
+    def __repr__(self):
+        return f"<namespace {self.fullname}{_docstr(self)}{_children(self)}>"
+
+    @cached_property
+    def members(self) -> dict[str, Doc]:
+        return self.bpy_members
+
+    #@cached_property
+    #def own_members(self) -> list[Doc]:
+    #    return self.members.values()
 
     @cached_property
     def is_package(self) -> bool:
         return True
 
     @cached_property
-    def members(self) -> dict[str, Doc]:
-        members: dict[str, Doc] = Module.members.func(self)
-        for name, doc in self.bpy_members.items():
-            members[name] = doc
+    def submodules(self) -> list[Module]:
+        return [x for x in self.members.values() if x.kind == 'module'  ]
 
+    @cached_property
+    def variables(self) -> list[Variable]:
+        return [x for x in self.members.values() if x.kind == 'variable']
+
+    @cached_property
+    def classes(self) -> list[Class]:
+        return [x for x in self.members.values() if x.kind == 'class'   ]
+    
+    @cached_property
+    def functions(self) -> list[Function]:
+        return [x for x in self.members.values() if x.kind == 'function']
+
+
+class BpyModule(BpyNamespace, Module):
+    def __init__(
+        self,
+        module: types.ModuleType,
+        rna_member_info: Iterable[(InfoMemberRNA, Doc)]
+    ):
+        BpyNamespace.__init__(self, module.__name__, rna_member_info)
+        Module.__init__(self, module)
+        self.bpy_module = module
+
+    @cached_property
+    def members(self) -> dict[str, Doc]:
+        members: dict[str, Doc] = {}#Module.members.func(self)
+        members.update(self.bpy_members)
         return members
 
     @cached_property
-    def submodules(self) -> list[Module]:
-        submodules = []
-        for doc in self.bpy_members.values():
-            if isinstance(doc, (Module, BpyModule)):
-                submodules.append(doc)
-        return submodules
+    def _var_docstrings(self) -> dict[str, str]:
+        return {}
+    
+    @cached_property
+    def _var_annotations(self) -> dict[str, Any]:
+        return {} 
 
 
-class BpyClass(RnaStruct):
+class BpyClass(RnaStruct, Class):
     def __init__(
         self, modulename: str, qualname: str, cls: type, taken_from: tuple[str, str], 
         rna_member_info: Iterable[InfoMemberRNA] = [],
@@ -197,12 +265,12 @@ class BpyClass(RnaStruct):
         
         for key, info in InfoFunctionRNA.global_lookup.items():
             if key[0] == self.rna_struct.identifier:
-                name, obj = self.object_from_rna_info(info)
+                name, obj = self._object_from_rna_info(info)
                 members[name] = obj
         
         for key, info in InfoPropertyRNA.global_lookup.items():
             if key[0] == self.rna_struct.identifier:
-                name, obj = self.object_from_rna_info(info)
+                name, obj = self._object_from_rna_info(info)
                 members[name] = obj
         
         return members
@@ -210,16 +278,8 @@ class BpyClass(RnaStruct):
     @cached_property
     def members(self) -> dict[str, Doc]:
         members: dict[str, Doc] = {} #Module.members.func(self)
-        for name, doc in self.bpy_members.items():
-            members[name] = doc
-
+        members.update(self.bpy_members)
         return members
-
-    @cached_property
-    def bases(self) -> list[tuple[str, str, str]]:
-        return []
-        bases = [self.rna_struct.base]
-        return bases
 
 
 class BpyParent(RnaInfo, Class):
@@ -244,11 +304,11 @@ class BpyParent(RnaInfo, Class):
 
     @cached_property
     def members(self) -> dict[str, Doc]:
-        members: dict[str, Doc] = {}
-        for name, doc in self.bpy_members.items():
-            members[name] = doc
+        return self.bpy_members
 
-        return members
+    @cached_property
+    def own_members(self) -> list[Doc]:
+        return list(self.members.values())
 
     @cached_property
     def bases(self) -> list[tuple[str, str, str]]:
@@ -256,25 +316,23 @@ class BpyParent(RnaInfo, Class):
 
     @cached_property
     def _rna_member_objects(self) -> dict[str, Any]:
-        members = {} #super()._rna_member_objects
+        members = RnaInfo._rna_member_objects.func(self)
         
         for key, info in InfoFunctionRNA.global_lookup.items():
             if key in self.ignore_rna_info_keys[1]:
                 continue
             if key[0] == self.rna_struct.identifier:
-                name, obj = self.object_from_rna_info(info)
+                name, obj = self._object_from_rna_info(info)
                 members[name] = obj
         
         for key, info in InfoPropertyRNA.global_lookup.items():
             if key in self.ignore_rna_info_keys[3]:
                 continue
             if key[0] == self.rna_struct.identifier:
-                name, obj = self.object_from_rna_info(info)
+                name, obj = self._object_from_rna_info(info)
                 members[name] = obj
         
         return members
-
-
 
 
 class BpyFunction(RnaStruct, Function):
@@ -296,18 +354,19 @@ class BpyFunction(RnaStruct, Function):
         props = self.rna_struct.properties
         args = []
         for prop in props:
-            if (prop.is_hidden):
+            if prop.is_hidden:
                 continue
+            prop_qualname = f'{self.fullname}.{prop.identifier}'
+            prop_doc = BpyProperty(self.modulename, prop_qualname, prop, self._taken_from(prop.identifier, prop))
             default = _safe_getattr(prop, 'default', empty)
             docstring += '<br/>'
             docstring += '<div class="classattr">'
-            docstring += '<div class="attr variable">'
-            docstring += f'<b>{prop.name}</b> (<span class="name">{prop.identifier}</span>: <span class="annotation">{prop.type}</span>'
-            if not default is empty:
-                docstring += f'<span class="default">= {default}</span>'
-            docstring += ')</div>'
-            if prop.description:
-                docstring += f"{prop.description}"
+            docstring +=     '<div class="attr variable">'
+            docstring +=         f'<span class="name">{prop_doc.name}</span>'
+            docstring +=         f'<span class="annotation">{prop_doc.annotation_str}</span>'
+            docstring +=         f'<span class="default">{prop_doc.default_value_str}</span>'
+            docstring +=     '</div>'
+            docstring +=     f'{prop_doc.function_docstring}'
             docstring += '</div>'
 
         return docstring
@@ -334,11 +393,7 @@ class BpyFunction(RnaStruct, Function):
     
     @cached_property
     def members(self) -> dict[str, Doc]:
-        members: dict[str, Doc] = {}
-        for name, doc in self.bpy_members.items():
-            members[name] = doc
-
-        return members
+        return self.bpy_members
 
 
 class BpyProperty(Variable):
@@ -350,11 +405,65 @@ class BpyProperty(Variable):
         taken_from: tuple[str, str],
         is_classvar: bool = True
     ):
+        self.bpy_prop = prop
         if (prop.is_hidden):
             docstring = prop.description
         else:
-            docstring = f'<h5>{prop.name}</h5>{prop.description}'
-        annotation = str(prop.type)
+            docstring = f'<h5>{self.rna_name}</h5>{self.rna_description}'
+        annotation = self._get_type(prop, taken_from)
         default_value = _safe_getattr(prop, 'default', empty)
         super().__init__(modulename, qualname, 
             taken_from=taken_from, docstring=docstring, annotation=annotation, default_value=default_value)
+
+    @cached_property
+    def function_docstring(self) -> type:
+        if not self.bpy_prop.is_hidden:
+            docstring = f'<h5>{self.rna_name}</h5> {self.rna_description}'
+        elif self.bpy_prop.description:
+            docstring = f'<h5>{self.rna_name} <i>(hidden)</i></h5> {self.rna_description}'
+        else:
+            docstring = ''
+        return docstring
+
+    @cached_property
+    def rna_name(self) -> type:
+        return self.bpy_prop.name
+
+    @cached_property
+    def rna_description(self) -> type:
+        return self.bpy_prop.description
+
+    @staticmethod
+    def _get_type(prop: bpy.types.Property, taken_from) -> type:
+        typedict = {
+            'BOOLEAN'    : bool,
+            'INT'        : int,
+            'FLOAT'      : float,
+            'STRING'     : str,
+            #'ENUM'       : Enum,
+            #'POINTER'    : ReferenceType[T],
+            #'COLLECTION' : dict[str, T],
+        }
+        if prop.type in typedict:
+            return typedict[prop.type]
+        elif prop.type == 'ENUM':
+            return BpyProperty._get_enum_type(prop, taken_from)
+        
+        #elif prop.type in ('POINTER', 'COLLECTION'):
+        struct_info = InfoStructRNA.global_lookup[('', prop.fixed_type.identifier)]
+        if prop.type == 'POINTER':
+            return struct_info.py_class
+        elif prop.type == 'COLLECTION':
+            return dict[str, struct_info.py_class]
+
+    @staticmethod
+    def _get_enum_type(prop, taken_from):
+            prop_enum_items: dict[str, Any] = {}
+            for enum_item in prop.enum_items_static.values():
+                prop_enum_items[enum_item.identifier] = enum_item.value
+            prop_enum: type = type(f'{prop.identifier}_enum', tuple(enum.Enum), prop_enum_items)
+            prop_enum.__module__ = taken_from[0]
+            prop_enum.__qualname__ = f'{taken_from[1]}.{prop_enum.__name__}'
+            return prop_enum
+
+        
--- docs/make.py
+++ docs/make.py
@@ -9,7 +9,9 @@ from bpydoc import *
 
 
 import bpy
-import rna_info # blender module; must be imported after bpy
+# blender modules; must be imported after bpy
+import bpy_types
+import rna_info
 
 
 
@@ -17,8 +19,10 @@ def pdoc_bpy(module: ModuleType, out_dir: str | Path = None) -> Dict[str, Module
     """
     Render the documentation for all exported bpy operators.
     """
+    module_dir = Path(f'{module.__file__}/../').resolve()
     if not out_dir is None:
         out_dir = Path(out_dir)
+    
 
     # To find the module's additions, get RNA info, call register, 
     # then get RNA info again, and find what was added
@@ -37,9 +41,37 @@ def pdoc_bpy(module: ModuleType, out_dir: str | Path = None) -> Dict[str, Module
 
     # Store where each struct is to easily resolve it later
     struct_modules = { }
-    bpy_modules: Dict[ModuleType, set] = { }
+    bpy_modules: Dict[ModuleType | str, set] = { }
     loaded_modules = { }
     bpy_ops_sub_modules = set()
+
+    def _get_rna_base(rna_obj: bpy_types.RNAMeta | bpy_types.StructRNA):
+        while (rna_obj.bl_rna.base is not None):
+            rna_obj = rna_obj.bl_rna.base
+        rna_base = getattr(bpy.types, rna_obj.identifier)
+        return rna_base
+
+    def _find_draw_funcs(rna_meta: bpy_types.RNAMeta):
+        if not hasattr(rna_meta, 'draw') or not hasattr(rna_meta.draw, '_draw_funcs'):
+            return
+        draw_func_docs = set()
+        for i, draw_func in enumerate(rna_meta.draw._draw_funcs):
+            file_path = Path(draw_func.__code__.co_filename)
+            if (file_path.is_relative_to(module_dir)):
+                func_doc = doc.Function(bpy.types.__name__, 
+                                        f'{rna_meta.bl_rna.identifier}.draw.draw_funcs[{i}]',
+                                        draw_func,
+                                        (draw_func.__module__, draw_func.__qualname__))
+                draw_func_docs.add(func_doc)
+        if len(draw_func_docs) > 0:
+            bpyparent = BpyParent(bpy_types, rna_meta.bl_rna, (rna_meta.__module__, rna_meta.__qualname__), draw_func_docs)
+            rna_base = _get_rna_base(rna_meta)
+            bpy_modules.setdefault(f'{rna_base.__module__}.{rna_base.__qualname__}', set()).add(bpyparent)
+            #print(bpyparent)
+    
+    for attr in dir(bpy.types):
+        _find_draw_funcs(getattr(bpy.types, attr))
+
 
     def _get_sub_module(module_name):
         return loaded_modules.setdefault(f'bpy.ops.{module_name}', getattr(bpy.ops, module_name))
@@ -79,6 +111,7 @@ def pdoc_bpy(module: ModuleType, out_dir: str | Path = None) -> Dict[str, Module
             bpyparent.rna_member_info.add(child_info)
             print(f"Got bpyparent {bpyparent}")
 
+
     for key, func_info in module_rna_info_list[1].items():
         _set_parent(key, func_info)
     for key, prop_info in module_rna_info_list[3].items():
@@ -86,16 +119,21 @@ def pdoc_bpy(module: ModuleType, out_dir: str | Path = None) -> Dict[str, Module
     for struct_info in module_rna_info_list[0].values():
         bpy_modules.setdefault(bpy.types, set()).add(struct_info)
         struct_modules[struct_info.identifier] = bpy.types
+        _find_draw_funcs(struct_info)
     for op_info in module_rna_info_list[2].values():
         sub_module = _get_sub_module(op_info.module_name)
         bpy_modules.setdefault(sub_module, set()).add(op_info)
         bpy_ops_sub_modules.add(sub_module)
         struct_modules[op_info.identifier] = sub_module
 
+
     all_modules = {}
     for bpy_module, rna_members in bpy_modules.items():
-        pdoc_module = BpyModule(bpy_module, rna_members)
-        all_modules[bpy_module.__name__] = pdoc_module
+        if isinstance(bpy_module, ModuleType):
+            pdoc_module = BpyModule(bpy_module, rna_members)
+        else:
+            pdoc_module = BpyNamespace(str(bpy_module), rna_members)
+        all_modules[pdoc_module.fullname] = pdoc_module
         #print(f'all_modules[{bpy_module.__name__}] = {pdoc_module}')
     
     # handle bpy.ops
@@ -121,8 +159,11 @@ def pdoc_blender_add_on(module: ModuleType, out_dir: Path | str):
     all_modules.update(api_docs.items())  
 
     module_dir = Path(f'{module.__file__}/../').resolve()
+    main_modules = {}
     for module_name in extract.walk_specs([str(module_dir)]):
-        all_modules[module_name] = doc.Module.from_name(module_name)  
+        main_modules[module_name] = doc.Module.from_name(module_name)  
+
+    all_modules.update(main_modules)
 
     out_dir = Path(out_dir)
     for pdoc_module in all_modules.values():
@@ -134,13 +175,31 @@ def pdoc_blender_add_on(module: ModuleType, out_dir: Path | str):
             outfile.parent.mkdir(parents=True, exist_ok=True)
             outfile.write_bytes(out.encode())
     
-    index_modules = {}
-    index_modules_shown = ['bpy.ops', 'bpy.types', module.__name__]
-    for module_shown in index_modules_shown:
-        index_modules[module_shown] = all_modules[module_shown]
+    print(all_modules.keys())
 
-    index = render.html_index(index_modules_shown)
+    index_names = {
+        'bpy_types.Header': 'Headers'      ,
+        'bpy_types.Panel' : 'Panels'       ,
+        'bpy_types.Menu'  : 'Menus'        ,
+        'bpy.ops'         : 'Operators'    ,
+        'bpy.types'       : 'Types'        ,
+        module.__name__   : module.__name__,
+    }
+    index_modules = { k: all_modules[k] for k in index_names }
+
+    for submodule in all_modules.values():
+        if submodule in index_modules.values():
+            continue
+        for index_module in index_modules.values():
+            if submodule in index_module.submodules:
+                break
+        else: # if loop did not break
+            if submodule not in main_modules.values():
+                print(f"WARN: Unreferenced submodule '{submodule.fullname}'")
+    index = render.html_index(index_modules)
     if index:
+        for old_name, new_name in index_names.items():
+            index = index.replace(old_name, new_name)
         (out_dir / "index.html").write_bytes(index.encode())
 
     search = render.search_index(all_modules)
