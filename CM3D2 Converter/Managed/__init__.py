fe0f713d8194d2978d6493b45f2e5c82217faeea
luvoid <72511739+luvoid@users.noreply.github.com>
2023-07-18 11:46:39 -0400

    Attempt to fix network path bug again

--- CM3D2 Converter/Managed/__init__.py
+++ CM3D2 Converter/Managed/__init__.py
@@ -7,6 +7,11 @@ from pathlib import Path as _Path
 from typing import TYPE_CHECKING as _TYPE_CHECKING
 
 _MANAGED_DIR = _Path(__file__).parent.absolute()
+print(f"_MANAGED_DIR = {_MANAGED_DIR}")
+if str(_MANAGED_DIR).startswith('file:'):
+    # Must get rid of the file:/// prefix!
+    _MANAGED_DIR = _Path(*_MANAGED_DIR.parts[1:])
+print(f"final _MANAGED_DIR = {_MANAGED_DIR}")
 
 import pythonnet as _pythonnet
 _pythonnet.set_runtime('netfx')
